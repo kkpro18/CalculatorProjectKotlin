3a44622178ed7891eee39f82926bccedf565a763
Kirisan Thiruchelvam <73609371+kkpro18@users.noreply.github.com>
2025-02-17 13:00:12 +0000

    Fixes now works, before it was comparing null pizza worth's so always equal

--- src/PizzaCalcNONNULL.kt
+++ src/PizzaCalcNONNULL.kt
@@ -4,16 +4,18 @@ class Pizza {
 
     var size : Double
     var price : Double
+    var worth: Double = 0.0
 
     init {
-        size = askInput("Enter your pizza size (inches) ").toDouble()
-        price = askInput("Enter the price of the pizza (£) ").toDouble()
+        size = askInput("Enter your pizza size (inches): ").toDouble()
+        price = askInput("Enter the price of the pizza (£): ").toDouble()
+        worth = calculatePizzaPerCurrency()
     }
 
     private fun askInput(question: String): String {
         var answer: String
         while (true) {
-            println(question)
+            print(question)
             answer = readln()
             if (answer.toDoubleOrNull() is Double && answer.toDouble() > 0.0){
                 break
@@ -24,31 +26,22 @@ class Pizza {
         return answer
     }
 
-    fun calculatePizzaPerCurrency(): Double {
+    private fun calculatePizzaPerCurrency(): Double {
         val pizzaArea = (size / 2).pow(2).times(Math.PI)
         val pizzaWorth = pizzaArea.div(price)
-//        println("worth $pizzaWorth")
-//        println("area $pizzaArea")
         return pizzaWorth
     }
-
-//    fun multiplePizzas(numberOfPizzas: Int){
-//        if (numberOfPizzas > 1){
-//
-//        }
-//
-//    }
 }
 
-fun whichIsBetterDeal(pizza1Size: Double, pizza2Size: Double, pizza1Price: Double, pizza2Price: Double, pizza1Worth: Double, pizza2Worth: Double){
-    if (pizza1Worth == pizza2Worth) {
+fun whichIsBetterDeal(pizza1: Pizza, pizza2: Pizza){
+    if (pizza1.worth == pizza2.worth) {
         println("Both Pizza Choices are Equal in Value")
-    } else if (pizza1Worth > pizza2Worth){
-        val extraPizzaPercentage = (pizza1Worth - pizza2Worth) / pizza2Worth * 100
-        println("Pizza 1 ($pizza1Size inches for £$pizza1Price) is better value for money, giving you ${String.format("%.2f",extraPizzaPercentage)}% more pizza.")
-    } else if (pizza2Worth > pizza1Worth){
-        val extraPizzaPercentage = (pizza2Worth - pizza1Worth) / pizza1Worth * 100
-        println("Pizza 2 ($pizza2Size inches for £$pizza2Price) is better value for money, giving you ${String.format("%.2f",extraPizzaPercentage)}% more pizza.")
+    } else if (pizza1.worth > pizza2.worth){
+        val extraPizzaPercentage = (pizza1.worth - pizza2.worth) / pizza2.worth * 100
+        println("Pizza 1 (${pizza1.size} inches for £${pizza1.price}) is better value for money, giving you ${String.format("%.2f",extraPizzaPercentage)}% more pizza.")
+    } else if (pizza2.worth > pizza1.worth){
+        val extraPizzaPercentage = (pizza2.worth - pizza1.worth) / pizza1.worth * 100
+        println("Pizza 2 (${pizza2.size} inches for £${pizza2.price}) is better value for money, giving you ${String.format("%.2f",extraPizzaPercentage)}% more pizza.")
     } else {
         println("Pizza Error Occured :<")
     }
@@ -57,9 +50,7 @@ fun whichIsBetterDeal(pizza1Size: Double, pizza2Size: Double, pizza1Price: Doubl
 fun main() {
     val pizza1 = Pizza()
     val pizza2 = Pizza()
-    val pizza1Worth = pizza1.calculatePizzaPerCurrency()
-    val pizza2Worth = pizza2.calculatePizzaPerCurrency()
-    whichIsBetterDeal(pizza1.size, pizza2.size, pizza1.price, pizza2.price, pizza1Worth, pizza2Worth)
+    whichIsBetterDeal(pizza1, pizza2)
 
 //    println(pizza1.pizzaSize)
 //    println(pizza1.pizzaPrice)
